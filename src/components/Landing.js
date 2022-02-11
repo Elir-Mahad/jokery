cd4e0c47a0df823b556da8b621e1b5474021294d
elir-mahad <mmahad280@gmail.com>
2022-02-11 12:48:12 -0500

    enhance(landing): add jokecard component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./lvl2components/Header";
+import JokeCard from "./lvl2components/JokeCard";
 
 function Landing() {
   return (
     <div>
       <Header />
+      <JokeCard />
     </div>
   );
 }
