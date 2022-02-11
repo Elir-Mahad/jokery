01ce50ed8537b3c18fc17d0b20f9275a1bc49367
elir-mahad <mmahad280@gmail.com>
2022-02-11 09:36:57 -0500

    enhance(app.js): import landing and settings components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Landing from "./components/Landing";
+import Settings from "./components/Settings";
 
 function App() {
   return (
     <div className="App">
       <h1>Jokery</h1>
+      <Landing />
+      <Settings />
     </div>
   );
 }
