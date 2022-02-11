033d6bbb63f4a2f077f768e927b6c6f102af9f9d
elir-mahad <mmahad280@gmail.com>
2022-02-11 15:22:03 -0500

    enhance(app.js): remove settings

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
-import Settings from "./components/Settings";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/settings" element={<Settings />} />
           <Route path="/" element={<Landing />} />
         </Routes>
       </Router>
