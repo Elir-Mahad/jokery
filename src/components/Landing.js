335eb6b12bdf15bad0370f922cbb434346f3aaa2
elir-mahad <mmahad280@gmail.com>
2022-02-11 10:05:54 -0500

    enhance(landing): add header

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./lvl2components/Header";
 
 function Landing() {
   return (
     <div>
       <h1>Landing page</h1>
+      <Header />
     </div>
   );
 }
