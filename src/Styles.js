280a7b5c1936fbd551bdeeccb997648bfaddef74
elir-mahad <mmahad280@gmail.com>
2022-02-11 10:18:22 -0500

    style(styles.ja): add ttl's for header

--- src/Styles.js
+++ src/Styles.js
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+export const HeaderBox = styled.div`
+  //
+`;
+
+export const LogoSettings = styled.div`
+  //
+`;
+
+export const Logo = styled.img`
+  //
+`;
+
+export const Settings = styled.button`
+  //
+`;
+
+export const Heading = styled.h2`
+  //
+`;
