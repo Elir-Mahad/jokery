a9c4fb7633716822bdcd4156b7be7860343cf632
elir-mahad <mmahad280@gmail.com>
2022-02-11 20:55:52 -0500

    enhance(app.js): remove react-router and landing import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </Router>
+      <h1>joke</h1>
     </div>
   );
 }
