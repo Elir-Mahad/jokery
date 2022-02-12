e308fc1752d12cab0b502108e9d44a73acb3129a
elir-mahad <mmahad280@gmail.com>
2022-02-12 06:28:47 -0500

    enhance(settingscard): add settingcontainer usestate

--- src/components/SettingsCard.js
+++ src/components/SettingsCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SettingsPage,
   SetBox,
@@ -20,6 +20,9 @@ import {
 import { FiTwitter } from "react-icons/fi";
 
 function SettingsCard() {
+  //
+  const [settingcontainer, setSettingContainer] = useState(true);
+  //
   return (
     <div>
       <SettingsPage>
